e1f7ba069b1599e4e0da6659438b74d33e7b174e
Chris <pschorr.christian@gmail.com>
2018-01-17 23:42:16 +0100

    #10: Einfache Übergangsanimation erstellt

--- Resources/Public/Js/application.js
+++ Resources/Public/Js/application.js
@@ -1,8 +1,24 @@
 // require('./Vendor/Salvattore');
 require('./Vendor/SlickSlider');
 
-$(".slider").slick({
+var slider = $('.slider');
 
-	// normal options...
-	infinite: false
+slider.slick({
+	infinite: false,
+	arrows: false,
+	waitForAnimate: false,
+	speed: 0,
+	fade: true
+});
+
+$('#prev').on('click', function() {
+
+});
+
+$('#next').on('click', function() {
+	slider.find('.slick-active').addClass('slick-slide-animate-out');
+
+	setTimeout(function() {
+		slider.slick('slickNext');
+	}, 1000);
 });
