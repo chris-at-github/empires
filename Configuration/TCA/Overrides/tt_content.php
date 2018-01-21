a14f973b77404d96b7c1a96f4660ec460b98d38d
Chris <pschorr.christian@gmail.com>
2018-01-21 11:15:19 +0100

    #11: Überschreiben der TCA Felddefinitionen über columnsOverride

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -11,24 +11,29 @@
     'datamints_play'
  );
 
- // Configure the default backend fields for the content element
- $GLOBALS['TCA']['tt_content']['types']['play_example'] = array(
-    'showitem' => '
-          --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.general;general,
-          --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.header;header,bodytext,image,
-       --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:tabs.appearance,
-          --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.frames;frames,
-       --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:tabs.access,
-          --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.visibility;visibility,
-          --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.access;access,
-       --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:tabs.extended
- ');
+	$GLOBALS['TCA']['tt_content']['types']['play_example'] = [
+		'showitem' => '
+				--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.general;general,
+				--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.header;header,bodytext,image,
+			--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:tabs.appearance,
+				--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.frames;frames,
+			--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:tabs.access,
+				--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.visibility;visibility,
+				--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.access;access,
+			--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:tabs.extended
+	 ',
+		'columnsOverrides' => [
+			'image' => [
+				'config' => [
+					'maxitems' => 1,
+				]
+			],
 
-// $GLOBALS['TCA']['tt_content']['types']['datamintsplay_pageheader']['columnsOverrides']['bodytext']['config'] = [
-// 'enableRichtext' => 1,
-// 'richtextConfiguration' => 'default'
-// ];
-
-// $GLOBALS['TCA']['tt_content']['types']['datamintsplay_pageheader']['columnsOverrides']['assets']['config'] = [
-// 'maxitems' => 1,
-// ];
+			'bodytext' => [
+				'config' => [
+					'enableRichtext' => 1,
+					'richtextConfiguration' => 'default'
+				]
+			]
+		]
+	];
