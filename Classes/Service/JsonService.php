2ffae7cd8099220a08619ad9b65dda2fc6c54216
Chris <pschorr.christian@gmail.com>
2018-01-22 22:21:26 +0100

    #14: Auswertung von Kindoptionen anhand von Kindoptionen

--- Classes/Service/JsonService.php
+++ Classes/Service/JsonService.php
@@ -17,13 +17,28 @@ class JsonService {
 	 * @return string
 	 */
 	public function toJson($value, $options = []) {
-		if($value instanceof \TYPO3\CMS\Extbase\DomainObject\AbstractEntity) {
-			$value = $this->domainObjectToArray($value, $options);
+		if(gettype($value) === 'object') {
+			$value = $this->toArray($value, $options);
 		}
 
 //		DebuggerUtility::var_dump($value);
 
 		return json_encode($value);
+	}
+
+	/**
+	 * toArray
+	 *
+	 * @param mixed $value
+	 * @param array $options
+	 * @return array
+	 */
+	public function toArray($value, $options = []) {
+		if($value instanceof \TYPO3\CMS\Extbase\DomainObject\AbstractEntity) {
+			$value = $this->domainObjectToArray($value, $options);
+		}
+
+		return $value;
 	}
 
 	/**
@@ -37,21 +52,15 @@ class JsonService {
 		$data = [];
 		$properties = \TYPO3\CMS\Extbase\Reflection\ObjectAccess::getGettablePropertyNames($object);
 
-		foreach($properties as $propertyName => $propertyOptions) {
-
-			// keine Optionen angegeben
-			if(gettype($propertyOptions) === 'string') {
-				$propertyName = $propertyOptions;
-			}
-
-			if(empty($options) === true || in_array($propertyName, $options) === true) {
+		foreach($properties as $propertyName) {
+			if(empty($options) === true || isset($options[$propertyName]) === true || in_array($propertyName, $options) === true) {
 				$propertyValue = \TYPO3\CMS\Extbase\Reflection\ObjectAccess::getProperty($object, $propertyName);
 
 				if(gettype($propertyValue) === 'array' || gettype($propertyValue) === 'object') {
 
 					if($propertyValue instanceof \TYPO3\CMS\Extbase\DomainObject\AbstractEntity) {
-						if(gettype($propertyOptions) === 'array') {
-							$propertyValue = $this->toJson($propertyValue, $propertyOptions);
+						if(isset($options[$propertyName]) === true && gettype($options[$propertyName]) === 'array') {
+							$propertyValue = $this->toArray($propertyValue, $options[$propertyName]);
 
 						} else {
 							$propertyValue = \TYPO3\CMS\Extbase\Reflection\ObjectAccess::getProperty($propertyValue, 'uid');
