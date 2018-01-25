b62583ad5e16834528b72df0d1bc04510220da10
Chris <pschorr.christian@gmail.com>
2018-01-25 06:04:28 +0100

    #15: Anpassung Persistence Mapping in TypoScript; Erstellung Models für die Record Types

---
+++ Classes/Domain/Model/DefaultExampleProperty.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Cext\Play\Domain\Model;
+
+/***
+ *
+ * This file is part of the "Play" Extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ *  (c) 2018
+ *
+ ***/
+
+/**
+ * DefaultExampleProperty
+ */
+class DefaultExampleProperty extends ExampleProperties {
+}
