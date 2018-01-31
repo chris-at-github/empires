23f87ebce635009e941f8dc9b3cfd14093265f2c
Chris <pschorr.christian@gmail.com>
2018-01-31 05:52:30 +0100

    #17: Ausgabe Übergabeparameter (Prop) Header

--- Resources/Public/Js/Components/ObjectListing.vue
+++ Resources/Public/Js/Components/ObjectListing.vue
@@ -1,11 +1,13 @@
 <template>
 	<div class="object-listing">
-		xxx
+		<header class="object-listing--header" v-if="header">{{header}}</header>
 	</div>
 </template>
 
 <script>
 	export default {
+		props: ['header'],
+
 		data: function() {
 			return {
 				settings:     {},
