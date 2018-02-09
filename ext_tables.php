3480c6a0c591ae32473271662610697415e55edf
Chris <pschorr.christian@gmail.com>
2018-02-09 06:18:19 +0100

    #17: Backend (ExtTables, TCA) Konfiguration für Beispiel Objekte VueObject angelegt

--- ext_tables.php
+++ ext_tables.php
@@ -21,6 +21,7 @@ call_user_func(function() {
 	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_play_domain_model_example');
 	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_play_domain_model_exampletype');
 	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_play_domain_model_exampleproperties');
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_play_domain_model_vueobject');
 });
 
 //\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
