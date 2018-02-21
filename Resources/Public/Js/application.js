73e0ba499b002ac441377177d18c4cab4501ed7c
Chris <pschorr.christian@gmail.com>
2018-02-21 22:33:03 +0100

    #18: Implementierung eines erfolgreichen API Get Calls

--- Resources/Public/Js/application.js
+++ Resources/Public/Js/application.js
@@ -1,7 +1,22 @@
-var Vue = require('vue');
+// var Vue = require('vue');
+//
+// Vue.component('vue-object-listing', require('./Components/VueObjectListing'));
+//
+// const application = new Vue({
+// 	el: '#vue-application'
+// });
 
-Vue.component('vue-object-listing', require('./Components/VueObjectListing'));
+$('.api-call--get').each(function() {
+	var container = $(this);
+	var url = $(this).data('api-url');
 
-const application = new Vue({
-	el: '#vue-application'
+	$.ajax({
+		url: url,
+		dataType: 'json',
+		success: function(response) {
+			container
+				.css('color', '#62a60a')
+				.text('\Cext\Play\Domain\Model\Example [' + response.uid + ']');
+		}
+	});
 });
