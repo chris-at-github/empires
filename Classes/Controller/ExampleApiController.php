dee9a7eb403ce81ac8fa8aec3c293c3096553f6d
Chris <pschorr.christian@gmail.com>
2018-02-21 20:42:07 +0100

    #18: Überschreiben der processRequest Methode um die automatische Verarbeitung der View zu verhindern, Setzen der richtigen Response Header

--- Classes/Controller/ExampleApiController.php
+++ Classes/Controller/ExampleApiController.php
@@ -28,26 +28,108 @@ class ExampleApiController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionContr
 	protected $exampleRepository = null;
 
 	/**
+	 * Get the TypoScriptFrontendController
+	 *
+	 * @return \TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController
+	 */
+	protected function getFrontendController() {
+		return $GLOBALS['TSFE'];
+	}
+
+	/**
+	 * Set response headers
+	 *
+	 * @see: \TYPO3\CMS\Extbase\Mvc\View\JsonView::render
+	 * @return void
+	 */
+	protected function setResponseHeader() {
+
+		$frontendController = $this->getFrontendController();
+		$response = $this->controllerContext->getResponse();
+
+		if($response instanceof \TYPO3\CMS\Extbase\Mvc\Web\Response) {
+
+			if($frontendController instanceof \TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController) {
+				if(empty($frontendController->config['config']['disableCharsetHeader'])) {
+
+					// If the charset header is *not* disabled in configuration,
+					// TypoScriptFrontendController will send the header later with the Content-Type which we set here.
+					$frontendController->setContentType('application/json');
+				} else {
+
+					// Although the charset header is disabled in configuration, we *must* send a Content-Type header here.
+					// Content-Type headers optionally carry charset information at the same time.
+					// Since we have the information about the charset, there is no reason to not include the charset information although disabled in TypoScript.
+					$response->setHeader('Content-Type', 'application/json; charset=' . trim($frontendController->metaCharset));
+				}
+			} else {
+				$response->setHeader('Content-Type', 'application/json');
+			}
+		}
+	}
+
+	/**
+	 * Handles a request. The result output is returned by altering the given response.
+	 *
+	 * @param \TYPO3\CMS\Extbase\Mvc\RequestInterface $request The request object
+	 * @param \TYPO3\CMS\Extbase\Mvc\ResponseInterface $response The response, modified by this handler
+	 *
+	 * @throws \TYPO3\CMS\Extbase\Mvc\Controller\Exception\RequiredArgumentMissingException
+	 * @throws \TYPO3\CMS\Extbase\Mvc\Exception\InvalidArgumentTypeException
+	 * @throws \TYPO3\CMS\Extbase\Mvc\Exception\NoSuchActionException
+	 * @throws \TYPO3\CMS\Extbase\Mvc\Exception\UnsupportedRequestTypeException
+	 */
+	public function processRequest(\TYPO3\CMS\Extbase\Mvc\RequestInterface $request, \TYPO3\CMS\Extbase\Mvc\ResponseInterface $response) {
+		if(!$this->canProcessRequest($request)) {
+			throw new \TYPO3\CMS\Extbase\Mvc\Exception\UnsupportedRequestTypeException(get_class($this) . ' does not support requests of type "' . get_class($request) . '". Supported types are: ' . implode(' ', $this->supportedRequestTypes), 1187701131);
+		}
+
+		if($response instanceof \TYPO3\CMS\Extbase\Mvc\Web\Response && $request instanceof WebRequest) {
+			$response->setRequest($request);
+		}
+
+		$this->response = $response;
+		$this->request = $request;
+		$this->request->setDispatched(true);
+
+		$this->uriBuilder = $this->objectManager->get(\TYPO3\CMS\Extbase\Mvc\Web\Routing\UriBuilder::class);
+		$this->uriBuilder->setRequest($request);
+
+		$this->actionMethodName = $this->resolveActionMethodName();
+		$this->initializeActionMethodArguments();
+		$this->initializeActionMethodValidators();
+
+		$this->mvcPropertyMappingConfigurationService->initializePropertyMappingConfigurationFromRequest($request, $this->arguments);
+		$this->initializeAction();
+
+		$actionInitializationMethodName = 'initialize' . ucfirst($this->actionMethodName);
+		if(method_exists($this, $actionInitializationMethodName)) {
+			call_user_func([$this, $actionInitializationMethodName]);
+		}
+
+		$this->mapRequestArgumentsToControllerArguments();
+		$this->controllerContext = $this->buildControllerContext();
+		$this->callActionMethod();
+
+		$this->setResponseHeader();
+	}
+
+	/**
 	 * Properties Action
 	 *
 	 * @param \Cext\Play\Domain\Model\Example $example
-	 * @return boolean
+	 * @return void
 	 */
 	public function getAction(\Cext\Play\Domain\Model\Example $example = null) {
 
-//		if($example === null && empty($this->settings['example']['uid']) === false) {
-//			$example = $this->exampleRepository->findByUid((int) $this->settings['example']['uid']);
-//		}
-
-//		// DomainObject -> toJson
-//		$this->objectManager->get(\Cext\Play\Service\JsonService::class)->toJson($example, [
-//			'uid',
-//			'state',
-//			'type' => ['uid', 'title'],
-//			'properties' => ['uid', 'title', 'value'],
-//			'date'
-//		]);
-
-		return true;
+		// DomainObject -> toJson
+		// @todo: statt echo sollten die Variablen ueber eine Set-Methode gesammelt werden und nach dem setzen der Header ausgegeben werden
+		echo $this->objectManager->get(\Cext\Play\Service\JsonService::class)->toJson($example, [
+			'uid',
+			'state',
+			'type' => ['uid', 'title'],
+			'properties' => ['uid', 'title', 'value'],
+			'date'
+		]);
 	}
 }
