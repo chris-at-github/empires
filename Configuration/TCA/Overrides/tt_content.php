621582400ed656c43743e5c945bfb1441d6c1af3
Chris <pschorr.christian@gmail.com>
2018-10-20 13:03:52 +0200

    #21: Erstellung der Grundstruktur

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -17,10 +17,59 @@ $tmpPlayTtContentColumns = array(
 			'eval' => ''
 		)
 	),
+
+	'tx_play_background' => [
+		'exclude' => true,
+		'label' => 'LLL:EXT:play/Resources/Private/Language/locallang_tca.xlf:tx_play_tt_content.background',
+		'config' =>
+			\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::getFileFieldTCAConfig(
+				'tx_play_background',
+				[
+					'appearance' => [
+						'createNewRelationLinkTitle' => 'LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:images.addFileReference'
+					],
+					'foreign_types' => [
+						'0' => [
+							'showitem' => '
+                        --palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
+                        --palette--;;filePalette'
+						],
+						\TYPO3\CMS\Core\Resource\File::FILETYPE_TEXT => [
+							'showitem' => '
+                        --palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
+                        --palette--;;filePalette'
+						],
+						\TYPO3\CMS\Core\Resource\File::FILETYPE_IMAGE => [
+							'showitem' => '
+                        --palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
+                        --palette--;;filePalette'
+						],
+						\TYPO3\CMS\Core\Resource\File::FILETYPE_AUDIO => [
+							'showitem' => '
+                        --palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
+                        --palette--;;filePalette'
+						],
+						\TYPO3\CMS\Core\Resource\File::FILETYPE_VIDEO => [
+							'showitem' => '
+                        --palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
+                        --palette--;;filePalette'
+						],
+						\TYPO3\CMS\Core\Resource\File::FILETYPE_APPLICATION => [
+							'showitem' => '
+                        --palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
+                        --palette--;;filePalette'
+						]
+					],
+					'maxitems' => 2
+				],
+				$GLOBALS['TYPO3_CONF_VARS']['GFX']['imagefile_ext']
+			),
+
+	],
 );
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tt_content', $tmpPlayTtContentColumns);
-//\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('tt_content', 'tx_datamints_biallo_visibility', '', 'after:section_frame');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('tt_content', 'tx_play_background', '', 'after:space_after_class');
 
 // ---------------------------------------------------------------------------------------------------------------------
 // Einfache Erweiterung, um die Moeglichkeiten von FSCE auszuprobieren
--- ext_tables.sql
+++ ext_tables.sql
@@ -2,7 +2,8 @@
 # Extend TtContent
 #
 CREATE TABLE tt_content (
-  tx_play_file int (11) unsigned DEFAULT '0' NOT NULL
+  tx_play_file int (11) unsigned DEFAULT '0' NOT NULL,
+  tx_play_background int (11) unsigned DEFAULT '0' NOT NULL
 );
 
 #
